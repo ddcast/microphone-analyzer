d20a284efdc76ad77547df0faa56a535375b8388
Devin <ddcast@gmail.com>
2014-03-30 17:20:11 -0400

    adding v1

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 microphone-analyzer
 ===================
+
+microphone-analyzer is a polymer element that enables microphone input data analysis on your documents.
+
+Dependencies:
+
+* (Polymer)[http://www.polymer-project.org/]
+* (Microphone)[https://github.com/srubin/microphone/]
+
+Usage:
+
+The `air` event is fired at the interaval specified by the unit option. `event.details` will contain the following properties:
+
+data
+rms
+value
+audioRange
+
+Example:
+
+```html
+<microphone-analyzer unit=".5" overlap=".25" channels="1">
+  <audio-range start="0" end="1"><em>speak up!</em></audio-range>
+  <audio-range start="1.01" end="2">quiet</audio-range>
+  <audio-range start="2.01" end="3">normal</audio-range>
+  <audio-range start="3.01" end="4">loud</audio-range>
+  <audio-range start="4.01" end="5">very loud</audio-range>
+</microphone-analyzer>
+```
+
+```javascript
+var microphoneAnalyzer = document.querySelector('microphone-analyzer');
+
+microphoneAnalyzer.valuefilter = function (rms) {
+  var vol = rms * 100;
+
+  return vol;
+};
+
+microphoneAnalyzer.addEventListener('air', function(e) {
+  console.log(e.detail.value);
+});
+```
---
+++ microphone-analyzer.js
@@ -0,0 +1,134 @@
+/**
+* MicrophoneAnalyzerElement
+* A polymer element that enables microphone input data analysis on your documents.
+*
+* @method valuefilter
+* @param {Number} rms Computed rms value
+
+* @method createdCallback
+* Called when a <microphone-analyzer> element is created.
+*/
+
+;(function MicrophoneAnalyzerElement(Microphone) {
+
+  // handle global errors
+  var err;
+
+  if (!arguments[0]) {
+    err = new Error('Missing dependency: Microphone. Try `bower install srubin/microphone`');
+  }
+
+  try {
+    // Microphone library dependency
+    var _testWebkitAudio = new window.webkitAudioContext()
+      .createJavaScriptNode(2048, 1, 1);
+
+    delete _testWebkitAudio;
+  }
+  catch(e) {
+    err = new Error('Fatal: Device not supported.'); 
+  }
+
+  if (err) {
+    throw err;
+  }
+
+  // generates a handler that calls a function with a given ~context
+  function proxy(fn, ctxValue) {
+    return function proxyHandler() { fn.apply(ctxValue, arguments); }
+  }
+
+  function getNumAttr(element, name, defaultValue) {
+    return parseFloat(element.getAttribute(name) || defaultValue);
+  }
+
+  function withinRange(value, range) {
+    return (value >= range.value[0] && value <= range.value[1]);
+  }
+
+  // generates a handler that sets the current range if it has changed
+  function rangeSetter(v) {
+    return function rangeSetterHandler(range, index) {
+      if (withinRange(v, range)) {
+        this.audioRange = range;
+
+        if (this.lastRange !== range) {
+          this.lastRange = range;
+        }
+      }
+    };
+  }
+
+  function micInputHandler(data) {
+    // presently supporting one channel
+    data = 2 === data.length ? data[0] : data;
+
+    // rms calculation via https://github.com/srubin/microphone
+    var rms = data.map(function (d) { return d * d; });
+    rms = rms.reduce(function (t, s) { return t + s; });
+    rms = Math.sqrt(rms / data.length);
+
+    var value = this.valuefilter(rms);
+
+    // set current input descriptive range
+    this.ranges.forEach(proxy(rangeSetter(value), this));
+
+    var evt = new CustomEvent('air', {
+      detail: {
+        data: data,
+        rms: rms,
+        value: value,
+        audioRange: this.audioRange
+      }
+    });
+
+    this.dispatchEvent(evt);
+  }
+
+  var AudioRangePrototype = Object.create(HTMLElement.prototype);
+
+  document.registerElement('audio-range', {
+    prototype: AudioRangePrototype
+  });
+
+  var MicrophoneAnalyzerPrototype = Object.create(HTMLElement.prototype);
+
+  MicrophoneAnalyzerPrototype.valuefilter = function valuefilter(rms) {
+    return rms;
+  };
+
+  MicrophoneAnalyzerPrototype.createdCallback = function createdCallback() {
+    var ranges = this.ranges = [];
+
+    this.audioRange = {};
+    this.lastRange = {};
+
+    this.unit = getNumAttr(this, 'unit', .5);
+    this.overlap = getNumAttr(this, 'overlap', .5);
+    this.channels = getNumAttr(this, 'channels', 1);
+
+    this.style.display = 'none';
+
+    this.querySelectorAll('audio-range').array()
+      .forEach(function (element, index) {
+        element.value = {
+          index: index,
+          innerHTML: element.innerHTML,
+          value: [ getNumAttr(element, 'start'), getNumAttr(element, 'end') ]
+        };
+
+        ranges.push(element.value);
+      });
+
+    this.mic = new Microphone({
+        unit: this.unit,
+        overlap: this.overlap,
+        channels: this.channels
+    }, proxy(micInputHandler, this));
+  };
+
+  document.registerElement('microphone-analyzer', {
+    prototype: MicrophoneAnalyzerPrototype
+  });
+
+} (window.Microphone));
